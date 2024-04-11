2c38f7a5c660987c1a09b4f296c68816c1c77167
nabin1234567 <ynabinb073@gmail.com>
2024-04-11 16:59:19 +0930

    feature-y commited

--- calculator.py
+++ calculator.py
@@ -15,7 +15,7 @@ def divide(x, y):
 print("Select operation:")
 print("1. Add")
 print("2. Subtract")
-print("3. Multiply")
+print("3. Multiplies")
 print("4. Divide")
 
 choice = input("Enter choice (1/2/3/4): ")
